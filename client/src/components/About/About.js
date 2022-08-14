4a0a492e5061ef163a7f83239b26ca0d6370d575
Emna-Sellami <100349349+Emna-Sellami@users.noreply.github.com>
2022-08-14 15:00:50 +0100

    fgd

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React  from "react";
 import client from "../../assets/rating.png";
 import owner from "../../assets/owner.png";
+import Modale from "./Modal";
 import './About.css'
 
 export default function About() {
@@ -10,27 +11,36 @@ export default function About() {
       title: "Client Space",
       subTitle:
         "Discover more the spirit of the hotel to reserve your journey ",
+      
     },
     {
       icon: owner,
-      title: "Owner Space",
+      title: "Staff Space",
       subTitle:
         "This space is dedicated for the hotel staff",
+      button: Modale
     },
   ];
+  
   return (
     <div name='sections' id="services" style={{padding: '5rem 0', display:' flex' , gap: '1rem'}}>
-      {data.map((service, index) => {
-        return (
-          <div className="service" key={index}>
+          <div className="service">
             <div className="icon">
-              <img src={service.icon} alt="" style={{ height:' 2.4rem'}} />
+              <img src={client} alt="" style={{ height:' 2.4rem'}} />
             </div>
-            <h3>{service.title}</h3>
-            <p>{service.subTitle}</p>
+            <h3>Client Space</h3>
+            <p>Discover more the spirit of the hotel to reserve your journey</p>
+           </div>
+           <div className="service">
+           <div className="icon">
+             <img src={owner} alt="" style={{ height:' 2.4rem'}} />
+           </div>
+           <h3>Staff Space</h3>
+           <p>This space is dedicated for the hotel staff</p>
+           <Modale/>
           </div>
-        );
-      })}
+      
+
     </div>
   );
 }
---
+++ client/src/components/About/Modal.js
@@ -0,0 +1,51 @@
+import{Button,Modal} from'react-bootstrap';
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class Modale extends React.Component {
+  constructor()
+  {
+    super()
+    this.state={
+      show:false
+    }
+  } 
+  handleModal()
+  {
+    this.setState({show:!this.state.show})
+  }
+
+  render(){
+    return (
+    <div>
+      <Button onClick={()=>{this.handleModal()}}>Enter</Button>
+      <Modal show={this.state.show}>
+        <Modal.Header>Enter password</Modal.Header>
+        <Modal.Body>
+        <form> 
+            
+            <div className="form-group">
+                <input 
+                    className="form-control"
+                    type="text"
+                    placeholder="Password"
+                />
+            </div>
+        </form> 
+
+        <button onClick={()=>{this.handleModal()}} class="btn btn-success mt-4">
+            save
+          </button>
+
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={()=>{this.handleModal()}}>
+            close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  )}
+}
+
+export default Modale;
