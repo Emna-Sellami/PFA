d0afa8baafd34e8fb3b19b7de1953dbffd8c32df
nouha fersi <nouha.fersi@ensi-uma.tn>
2022-08-14 14:31:25 +0200

    ,

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -4,18 +4,23 @@ import owner from "../../assets/owner.png";
 import './About.css'
 
 export default function About() {
+  const handleClick =(x) => {
+    console.log(x)
+  }
   const data = [
     {
       icon: client,
       title: "Client Space",
       subTitle:
         "Discover more the spirit of the hotel to reserve your journey ",
+      button:"More",
     },
     {
       icon: owner,
       title: "Owner Space",
       subTitle:
         "This space is dedicated for the hotel staff",
+      button:"more",
     },
   ];
   return (
@@ -24,10 +29,11 @@ export default function About() {
         return (
           <div className="service" key={index}>
             <div className="icon">
-              <img src={service.icon} alt="" style={{ height:' 2.4rem'}} />
+              <img src={service.icon} alt="" style={{ height:' 2.4rem'}}/>
             </div>
             <h3>{service.title}</h3>
             <p>{service.subTitle}</p>
+            <button onClick={() => {handleClick(service.title) }}>More</button>
           </div>
         );
       })}
