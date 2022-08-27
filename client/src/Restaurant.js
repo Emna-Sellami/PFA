201e97ca8ac17fd937816c0f08230061838f8edf
inesfadhli <ines.fadhli@ensi-uma.tn>
2022-08-27 14:58:56 +0200

    rr

--- client/src/Restaurant.js
+++ client/src/Restaurant.js
@@ -1,14 +1,13 @@
 import Restau from "./pages/restau";
-import Navbar from "./components/Navbar/Navbar";
-import Footer from "./components/Footer/Footer";
+
 
 const Restaurant = () => {
   return (
+    
     <div>
-    <Navbar/>
     <Restau/> 
-    <Footer/>
     </div>
+  
   );
  };
   
--- client/src/pages/restau.js
+++ client/src/pages/restau.js
@@ -1,29 +1,47 @@
-import React from "react";
-import HeroImage from "../components/HeroImage/HeroImage";
-import bgImage from "../assets/coffee.jpg";
-import MenuOfTheDay from "../components/MenuOfTheDay/MenuOfTheDay";
-import Breakfast from '../components/Breakfast/Breakfast'
-import LunchTime from "../components/LunchTime/LunchTime";
-import CoffeeSelection from '../components/CoffeeSelection/CoffeeSelection'
-import Beverages from '../components/Beverages/Beverages'
+import React, { useState } from "react";
+import Menu from "../components/menu";
+import Categories from "../components/categories";
+import items from "../components/data";
+import logo from "../assets/logo.JPG";
+import "../assets/css/Restau.css"
 
-class Restau extends React.Component {
-    render(){
-        return (
-            <div>
-              <HeroImage
-                bgImage={bgImage}
-                heading={["Our ", <span>Menu</span>]}
-                text="Everything we have to offer at one glance"
-              />
-              <MenuOfTheDay />
-              <Breakfast />
-              <LunchTime />
-              <CoffeeSelection />
-              <Beverages />
-            </div>
-          );
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
 
-    }};
+export default function Restau () {
 
-export default Restau;
+  const [menuItems, setMenuItems] = useState(items);
+  const [activeCategory, setActiveCategory] = useState("");
+  const [categories, setCategories] = useState(allCategories);
+  const filterItems = (category) => {
+    setActiveCategory(category);
+    if (category === "all") {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+  return (
+    
+      <div >
+        <div className="title">
+          <img src={logo} alt="logo" className="logo" />
+          <h2>Menu List</h2>
+          <div className="underline"></div>
+        </div>
+        <div className="menu-items">
+        <Categories
+          categories={categories}
+          activeCategory={activeCategory}
+          filterItems={filterItems}
+        />
+        </div>
+        <div className="menu-items">
+           <Menu items={menuItems} />
+          </div>
+      </div>
+
+  );
+};
+
+
